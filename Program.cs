2e5eeee1b204ad91bfe252a8769fbd5573780171
pranali2808 <ranepranali1997@gmail.com>
2022-03-31 10:01:01 +0530

    UC1 LineLength

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+            LineComparision lineComparison = new LineComparision();
+            lineComparison.PointValues();
+            double lineLength = lineComparison.GetLength();
+            Console.WriteLine("Length of first Line according to points is= " + lineLength);
+            Console.ReadLine();
+        }
+    }
+}
